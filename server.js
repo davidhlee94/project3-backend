9f884fdad29916a9a4b0557812e045e1f3378f73
luigibustos <louisalphonsobustos@gmail.com>
2023-01-03 14:27:54 -0800

    Configured server.js and db.connection.js files

---
+++ server.js
@@ -0,0 +1,30 @@
+// IMPORT DOTENV
+require("dotenv").config();
+
+const { PORT } = process.env;
+
+// IMPORT EXPRESS
+const express = require("express");
+const app = express();
+
+// IMPORT MONGODB CONNECTION
+require("./config/db.connection");
+
+// IMPORT MORGAN AND CORS
+const cors = require("cors");
+const morgan = require("morgan");
+
+// MIDDLEWARE
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json()); // app.use(express.json()) MUST GO BEFORE THE CONTROLLERS
+app.use((err, req, res, next) => res.status(500).send(err));
+// TEST ROUTE -> WORKING
+app.get("/", (req, res) => {
+  res.send("Sanity Check for Project 3");
+});
+
+// LISTEN ROUTE
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
